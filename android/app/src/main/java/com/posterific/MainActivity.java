ca2db6cedee59b4c68f107dd663e9d5fc45c909c
John Otu <johnne01@gmail.com>
2017-10-09 08:11:01 +0000

    Completed Lab 2 Changes to be committed: 	modified:   android/app/build.gradle 	modified:   android/app/src/main/AndroidManifest.xml 	modified:   android/app/src/main/java/com/posterific/MainActivity.java 	modified:   android/app/src/main/java/com/posterific/MainApplication.java 	modified:   android/app/src/main/res/values/strings.xml 	modified:   android/settings.gradle 	modified:   package.json

--- android/app/src/main/java/com/posterific/MainActivity.java
+++ android/app/src/main/java/com/posterific/MainActivity.java
@@ -1,5 +1,7 @@
 package com.posterific;
 
+import android.content.Intent;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -10,6 +12,13 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
+
         return "posterific";
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
--- android/app/src/main/java/com/posterific/MainApplication.java
+++ android/app/src/main/java/com/posterific/MainApplication.java
@@ -2,18 +2,28 @@ package com.posterific;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 import com.facebook.react.ReactApplication;
-import fr.greweb.reactnativeviewshot.RNViewShotPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
+
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager(){
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -25,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new RNViewShotPackage(),
             new PickerPackage()
       );
@@ -39,6 +50,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
